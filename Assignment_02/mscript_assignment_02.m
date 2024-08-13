3093dc3cf8d9dfdf8ad66360f07bafdff92b21eb
AdityaNijave <adityanijave09@gmail.com>
2024-08-13 21:57:45 +0530

    Sol. of Assignment 02

---
+++ Assignment_02/mscript_assignment_02.m
@@ -0,0 +1,5 @@
+Ti = input("enter the value for Ti: \n");
+A = input("enter the value for A: \n");
+F = input("enter the value of F: \n");
+sim("simfile_assignment_02.slx");
+disp("Program is exe.");
