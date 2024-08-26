9162d4e4a678d8a6eeffa513d4bb11cf219ceb01
AdityaNijave <adityanijave09@gmail.com>
2024-08-26 11:11:42 +0530

    learning the m-scripting and model desgining for the damping factor using its equation and testing under verious values of damping factor like B = 0, B < 1 and B > 1

---
+++ Assignment_05/assignment_05_run.m
@@ -0,0 +1,10 @@
+% taking input from user
+
+F = input("enter the value of Force: ");
+M = input("enter the value of Mass: ");
+B = input("enter the value of Dampaing factor: ");
+K = input("enter the value of K (Spring constant): ");
+
+sim("assignment_05.slx");
+
+disp("program exe. completed");
