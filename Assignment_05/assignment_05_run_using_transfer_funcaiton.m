434b0111e87aaf5eff1ef65b8a07a1681b5b591d
AdityaNijave <adityanijave09@gmail.com>
2024-08-26 12:08:22 +0530

    learning the m-scripting and model desgining using tranfer funcation block  for the damping factor using its equation and testing under verious values of damping factor like B = 0, B < 1 and B > 1

---
+++ Assignment_05/assignment_05_run_using_transfer_funcaiton.m
@@ -0,0 +1,11 @@
+% taking input from user
+
+F = input("enter the value of Force: ");
+M = input("enter the value of Mass: ");
+B = input("enter the value of Dampaing factor: ");
+K = input("enter the value of K (Spring constant): ");
+
+%sim("assignment_05.slx");
+sim("assignment_05_using_transfer_funcation.slx");
+
+disp("program exe. completed");
